cf60e897718003511afc0622af66f2dd2089e056
Raghav Luthra <rluthra2002@gmail.com>
2023-04-29 11:19:03 +0530

    Add functions for creating new node and rtree.

--- rtree.c
+++ rtree.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 
 #define DIMS 2
@@ -38,6 +39,40 @@ struct rtree {
     NODE root;
     MBR rect;
 };
+
+
+NODE createNewNode(bool isLeaf) {
+    NODE myNode = (NODE) malloc(sizeof(struct node));
+    memset(myNode, 0, sizeof(myNode));
+    myNode->isLeaf = isLeaf;
+    return myNode;
+}
+
+RTREE createNewRtree() {
+    RTREE myRtree = (RTREE) malloc(sizeof(struct rtree));
+    memset(myRtree, 0, sizeof(myRtree));
+    return myRtree;
+}
+
+// function to find the MBR for a given node
+// MBR findMBR(NODE node) {
+//     if(node->numChildren == 0) {
+//         printf("Error: no children in given node!");
+//     }
+
+//     int x1, x2, y1, y2;
+
+//     // for leaf node
+//     if(isLeaf) {
+//         for(int i = 0; i < node->numChildren; i++) {
+            
+//         }
+//     }
+
+//     // for internal node
+// }
+
+
 
 
 int main() {
