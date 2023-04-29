5eb029c34b929b7d35bfbb8098992b9910ef2e58
Raghav Luthra <rluthra2002@gmail.com>
2023-04-29 13:01:35 +0530

    Change structure of MBR and add helper functions to merge and check intersection of rects.

--- rtree.c
+++ rtree.c
@@ -22,14 +22,13 @@ int max(int a, int b) {
 
 typedef struct mbr* MBR;
 struct mbr {
-    int x[DIMS];
-    int y[DIMS];
+    int bottomLeft[DIMS];
+    int topRight[DIMS];
 };
 
 typedef struct item* ITEM;
 struct item {
-    int x;
-    int y;
+    int data[DIMS];
 };
 
 typedef struct node* NODE;
@@ -67,57 +66,77 @@ RTREE createNewRtree() {
 }
 
 // function to find the MBR for a given node
-MBR findMBR(NODE node) {
-    if(node->numChildren == 0) {
-        printf("Error: no children in given node!");
+// MBR findMBR(NODE node) {
+//     if(node->numChildren == 0) {
+//         printf("Error: no children in given node!");
+//     }
+
+//     int x0 = INT_MIN, x1 = INT_MAX, y0 = INT_MIN, y1 = INT_MAX;
+
+//     // for leaf node
+//     if(node->isLeaf) {
+//         for(int i = 0; i < node->numChildren; i++) {
+//             ITEM item = node->items[i];
+//             int data_x = item->x;
+//             int data_y = item->y;
+//             x0 = min(x0, data_x);
+//             y0 = min(y0, data_y);
+//             x1 = max(x1, data_x);
+//             y1 = max(y1, data_y);
+//         }
+
+//         MBR rect = (MBR) malloc(sizeof(struct mbr));
+//         rect->x[0] = x0;
+//         rect->x[1] = x1;
+//         rect->y[0] = y0;
+//         rect->y[1] = y1;
+
+//         return rect;
+//     }
+
+//     // for internal node
+//     else {
+//         for(int i = 0; i < node->numChildren; i++) {
+//             MBR childRect = node->rects[i];
+//             int data_x0 = childRect->x[0];
+//             int data_y0 = childRect->y[0];
+//             int data_x1 = childRect->x[1];
+//             int data_y1 = childRect->y[1];
+
+//             x0 = min(x0, data_x0);
+//             y0 = min(y0, data_y0);
+//             x1 = max(x1, data_x1);
+//             y1 = max(y1, data_y1);
+//         }
+
+//         MBR rect = (MBR) malloc(sizeof(struct mbr));
+//         rect->x[0] = x0;
+//         rect->x[1] = x1;
+//         rect->y[0] = y0;
+//         rect->y[1] = y1;
+
+//         return rect;
+//     }
+// }
+
+
+bool rectIntersects(MBR r1, MBR r2) {
+    for(int d = 0; d < DIMS; d++) {
+        if(r1->bottomLeft[d] > r2->topRight[d] || r1->topRight[d] < r2->bottomLeft[d])
+            return false;
     }
 
-    int x0 = INT_MIN, x1 = INT_MAX, y0 = INT_MIN, y1 = INT_MAX;
+    return true;
+}
 
-    // for leaf node
-    if(node->isLeaf) {
-        for(int i = 0; i < node->numChildren; i++) {
-            ITEM item = node->items[i];
-            int data_x = item->x;
-            int data_y = item->y;
-            x0 = min(x0, data_x);
-            y0 = min(y0, data_y);
-            x1 = max(x1, data_x);
-            y1 = max(y1, data_y);
-        }
-
-        MBR rect = (MBR) malloc(sizeof(struct mbr));
-        rect->x[0] = x0;
-        rect->x[1] = x1;
-        rect->y[0] = y0;
-        rect->y[1] = y1;
-
-        return rect;
+MBR mergeRect(MBR r1, MBR r2) {
+    MBR rect = (MBR) malloc(sizeof(struct mbr));
+    for(int d = 0; d < DIMS; d++) {
+        rect->bottomLeft[d] = min(r1->bottomLeft[d], r2->bottomLeft[d]);
+        rect->topRight[d] = max(r1->topRight[d], r2->topRight[d]);
     }
 
-    // for internal node
-    else {
-        for(int i = 0; i < node->numChildren; i++) {
-            MBR childRect = node->rects[i];
-            int data_x0 = childRect->x[0];
-            int data_y0 = childRect->y[0];
-            int data_x1 = childRect->x[1];
-            int data_y1 = childRect->y[1];
-
-            x0 = min(x0, data_x0);
-            y0 = min(y0, data_y0);
-            x1 = max(x1, data_x1);
-            y1 = max(y1, data_y1);
-        }
-
-        MBR rect = (MBR) malloc(sizeof(struct mbr));
-        rect->x[0] = x0;
-        rect->x[1] = x1;
-        rect->y[0] = y0;
-        rect->y[1] = y1;
-
-        return rect;
-    }
+    return rect;
 }
 
 
